3084c2ea245b9b6617e8ad01a35b9982b852064b
BoWen Chai <lovebaihezi@gmail.com>
2025-02-08 14:56:28 +0800

    fixup

---
+++ crates/app/src/txt.rs
@@ -0,0 +1,11 @@
+use bevy::prelude::*;
+
+/// # Bevy Plugin for rendering Text(No format text)
+
+pub fn setup_txt_render_system(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+}
+
+pub fn load_txt_from_local_file_system() {}
