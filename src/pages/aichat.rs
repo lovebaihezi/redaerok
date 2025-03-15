74a382201156c701d1be25e474f40665d849afda
BoWen Chai <lovebaihezi@gmail.com>
2025-03-15 07:17:28 -0400

    feat: AI Chat Leave

--- src/pages/aichat.rs
+++ src/pages/aichat.rs
@@ -7,17 +7,23 @@ pub struct AIChatPlugin;
 #[derive(Component)]
 pub struct AIChatRootInput;
 
+#[derive(Component)]
+pub struct AIChatRoot;
+
 fn ai_chat_input_base() -> impl Bundle {
-    (Node {
-        width: Val::Percent(100.0),
-        height: Val::Percent(100.0),
-        display: Display::Flex,
-        flex_direction: FlexDirection::Column,
-        justify_content: JustifyContent::Center,
-        align_items: AlignItems::Center,
-        border: UiRect::bottom(Val::Px(1.0)),
-        ..Default::default()
-    },)
+    (
+        AIChatRoot,
+        Node {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            display: Display::Flex,
+            flex_direction: FlexDirection::Column,
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            border: UiRect::bottom(Val::Px(1.0)),
+            ..Default::default()
+        },
+    )
 }
 
 pub fn init_ai_chat_page(mut commands: Commands, assets: ResMut<AssetServer>) {
@@ -29,6 +35,7 @@ pub fn init_ai_chat_page(mut commands: Commands, assets: ResMut<AssetServer>) {
                 width: Val::Px(128.0),
                 ..Default::default()
             },
+            BackgroundColor::from(Color::WHITE),
             BorderColor::from(Color::BLACK),
         ))
         .with_child(normal::bundle(
@@ -42,8 +49,15 @@ pub fn init_ai_chat_page(mut commands: Commands, assets: ResMut<AssetServer>) {
         ));
 }
 
+pub fn remove_aichat_page(mut commands: Commands, query: Query<Entity, With<AIChatRoot>>) {
+    for ent in query.iter() {
+        commands.entity(ent).despawn_recursive();
+    }
+}
+
 impl Plugin for AIChatPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(PageState::AIChatPage), init_ai_chat_page);
+        app.add_systems(OnEnter(PageState::AIChatPage), init_ai_chat_page)
+            .add_systems(OnExit(PageState::AIChatPage), remove_aichat_page);
     }
 }
