16e3564e3970c411fccbf40146fece02bd8a71ec
BoWen Chai <lovebaihezi@gmail.com>
2025-03-25 11:37:40 -0400

    fixup

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 pub mod app;
 mod camera;
 pub mod components;
-mod cool18bbs4;
 mod game_control;
 mod pages;
 pub mod resources;
@@ -9,5 +8,4 @@ mod states;
 pub mod test_functions;
 
 pub use camera::normal_camera;
-pub use cool18bbs4::*;
 pub use game_control::*;
